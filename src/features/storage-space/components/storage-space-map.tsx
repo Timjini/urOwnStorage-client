41ef7ca87238aadd46a5d0014a5b3fdf06dcb5d0
Timjini <hatim.jini@gmail.com>
2026-06-24 14:05:19 +0200

    missing apikey created the issue

--- src/features/storage-space/components/storage-space-map.tsx
+++ src/features/storage-space/components/storage-space-map.tsx
@@ -1,11 +1,27 @@
 import { AppleMaps, GoogleMaps } from "expo-maps";
-import { Platform, Text } from "react-native";
+import { Platform, Text, View } from "react-native";
 
-export default function App() {
+export default function StorageSpaceMap({
+  latitude,
+  longitude,
+}: {
+  latitude: number;
+  longitude: number;
+}) {
   if (Platform.OS === "ios") {
     return <AppleMaps.View style={{ flex: 1 }} />;
   } else if (Platform.OS === "android") {
-    return <GoogleMaps.View style={{ flex: 1 }} />;
+    return (
+      <View style={{ flex: 1, height: 400 }}>
+        <Text>Map is not available on Android!!!</Text>
+        <GoogleMaps.View
+          cameraPosition={{
+            coordinates: { latitude, longitude },
+            zoom: 12,
+          }}
+        />
+      </View>
+    );
   } else {
     return <Text>Maps are only available on Android and iOS</Text>;
   }
